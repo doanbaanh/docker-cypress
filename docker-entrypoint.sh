fb226afdb6f3cc4dda09ee488f75c95addcd9bb2
doanbaanh <doanbaanh@gmail.com>
2021-05-26 12:52:23 +0300

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 npm --prefix /e2e install /e2e
-/e2e/node_modules/.bin/cypress install
+/e2e/node_modules/.bin/cypress install --save-dev
 (cd /e2e && npx cypress open --config baseUrl=http://$BASE_URL)
